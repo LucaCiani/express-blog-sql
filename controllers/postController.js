2a2d53e8e55b7fdbc36aa55223efa73d60030bec
LucaCiani <lucaciani88@gmail.com>
2025-07-10 14:52:52 +0200

    add show function

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,16 +14,15 @@ function index(req, res) {
 
 // show: Dettagli di un post specifico
 function show(req, res) {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    if (post) {
-        res.json({ message: "Ecco i dettagli del post", post });
-    } else {
-        res.status(404).json({
-            error: "Not Found",
-            message: "Post non trovato",
-        });
-    }
+    const { id } = req.params;
+    const sql = "SELECT * FROM posts WHERE id = ?";
+    connection.query(sql, [id], (err, results) => {
+        if (err)
+            return res.status(500).json({ error: "Database query failed" });
+        if (results.length === 0)
+            return res.status(404).json({ error: "Post not found" });
+        res.json(results[0]);
+    });
 }
 
 // store: Creazione di un nuovo post
